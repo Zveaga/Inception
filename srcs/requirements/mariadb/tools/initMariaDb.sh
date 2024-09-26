c78eb0235945ca8b1bc6611f7c2e8721d1127efd
Rares <rares.rares.cox@gmail.com>
2024-09-26 16:31:27 +0200

    mariadb script done

---
+++ srcs/requirements/mariadb/tools/initMariaDb.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+cd /var/lib/mysql
+
+# if [ ! -d "/var/lib/mysql/$DB_NAME" ]; then
+
+	# if [ ! -f db.sql ]; then
+
+		echo "CREATE DATABASE IF NOT EXISTS $DB_NAME;" > db.sql
+		echo "CREATE USER IF NOT EXISTS '$DB_USER'@'%' IDENTIFIED BY '$DB_USER_PASSWORD';" >> db.sql
+		echo "ALTER USER '$DB_USER'@'%' IDENTIFIED BY '$DB_USER_PASSWORD';" >> db.sql
+		echo "GRANT ALL PRIVILEGES ON *.* TO '$DB_USER'@'%' IDENTIFIED BY '$DB_USER_PASSWORD';" >> db.sql
+		echo "FLUSH PRIVILEGES;" >> db.sql
+	# fi
+
+#	mariadb-install-db \
+#	--user=mysql \
+#	--datadir=/var/lib/mysql \
+#	--skip-test-db \
+#	--skip-name-resolve \
+#	--auth-root-authentication-method=normal
+
+	sleep 2
+
+	service mariadb start
+	mysql < db.sql
+
+	sleep 2
+	service mariadb stop
+# fi
+
+# echo "Starting mysqld_safe" >> /var/log/initMariaDb.log
+mysqld_safe
