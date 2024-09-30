ee2291d81ac347dbbe744c0308e6a9b6349fae5c
Rares <rares.rares.cox@gmail.com>
2024-09-30 19:37:03 +0200

    working on getting the env vars insite the wp-config.php

--- srcs/requirements/mariadb/tools/initMariaDb.sh
+++ srcs/requirements/mariadb/tools/initMariaDb.sh
@@ -23,3 +23,33 @@ fi
 
 # echo "Starting mysqld_safe" >> /var/log/initMariaDb.log
 mysqld_safe
+
+
+# #!/bin/bash
+
+# cd /var/lib/mysql
+
+# if [ ! -d "/var/lib/mysql/$DB_NAME" ]; then
+#     if [ ! -f db.sql ]; then
+#         echo "CREATE DATABASE IF NOT EXISTS $DB_NAME;" > db.sql
+#         echo "CREATE USER IF NOT EXISTS '$DB_USER'@'%' IDENTIFIED BY '$DB_USER_PASSWORD';" >> db.sql
+#         echo "GRANT ALL PRIVILEGES ON *.* TO '$DB_USER'@'%';" >> db.sql
+#         echo "FLUSH PRIVILEGES;" >> db.sql
+#     fi
+    
+#     # Wait a moment to ensure the service can start properly
+#     sleep 2
+    
+#     # Start the service and wait for it to be fully up
+#     service mariadb start
+#     sleep 5  # Adjust the sleep duration as needed for initialization
+    
+#     # Execute the SQL commands
+#     mysql < db.sql
+    
+#     # Stop the service
+#     service mariadb stop
+# fi
+
+# # Start the MariaDB server
+# mysqld_safe
