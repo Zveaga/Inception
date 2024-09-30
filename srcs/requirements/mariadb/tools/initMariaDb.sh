58b3ead690fe1ceb8bd7ef093f94a79310a0ac8b
Rares <rares.rares.cox@gmail.com>
2024-09-30 15:36:41 +0200

    index.html works but index.php says Error establishing a database connection

--- srcs/requirements/mariadb/tools/initMariaDb.sh
+++ srcs/requirements/mariadb/tools/initMariaDb.sh
@@ -2,16 +2,16 @@
 
 cd /var/lib/mysql
 
-# if [ ! -d "/var/lib/mysql/$DB_NAME" ]; then
+if [ ! -d "/var/lib/mysql/$DB_NAME" ]; then
 
-	# if [ ! -f db.sql ]; then
+	if [ ! -f db.sql ]; then
 
 		echo "CREATE DATABASE IF NOT EXISTS $DB_NAME;" > db.sql
 		echo "CREATE USER IF NOT EXISTS '$DB_USER'@'%' IDENTIFIED BY '$DB_USER_PASSWORD';" >> db.sql
 		echo "ALTER USER '$DB_USER'@'%' IDENTIFIED BY '$DB_USER_PASSWORD';" >> db.sql
 		echo "GRANT ALL PRIVILEGES ON *.* TO '$DB_USER'@'%' IDENTIFIED BY '$DB_USER_PASSWORD';" >> db.sql
 		echo "FLUSH PRIVILEGES;" >> db.sql
-	# fi
+	fi
 	sleep 2
 
 	service mariadb start
@@ -19,7 +19,7 @@ cd /var/lib/mysql
 
 	sleep 2
 	service mariadb stop
-# fi
+fi
 
 # echo "Starting mysqld_safe" >> /var/log/initMariaDb.log
 mysqld_safe
--- srcs/requirements/wordpress/tools/initWp.sh
+++ srcs/requirements/wordpress/tools/initWp.sh
@@ -1,38 +1,34 @@
 #!/bin/bash
 
-if [ ! -f ./wp-config.php ]; then 
+cd /var/www/html/
 
+if [ ! -f ./wp-config.php ]; then 
 	wget http://wordpress.org/latest.tar.gz
 	tar xfz latest.tar.gz
 	mv wordpress/* .
 	rm -rf latest.tar.gz
 	rm -rf wordpress
-
 	sed -i "s/username_here/$DB_USER/g" wp-config-sample.php
 	sed -i "s/password_here/$DB_USER_PASSWORD/g" wp-config-sample.php
 	sed -i "s/localhost/$DB_HOST/g" wp-config-sample.php
 	sed -i "s/database_name_here/$DB_NAME/g" wp-config-sample.php
 	cp wp-config-sample.php wp-config.php
-
 	chown -R www-data:www-data /var/www/html
 	chmod -R 755 /var/www/html
 	chown www-data:www-data wp-config.php
 	chmod 644 wp-config.php
-
 	wp core install --allow-root --url="$DOMAIN_NAME" --title=Inception \
         --admin_user="$WP_ADMIN" --admin_password="$WP_ADMIN_PASSWORD" \
         --admin_email="$WP_ADMIN_EMAIL" --skip-email
-
 	wp user create \
 	--allow-root \
 	"$WP_USER" \
 	"$WP_USER_EMAIL" \
         --role=author \
 	--user_pass="$WP_USER_PASSWORD"
-
 fi
 
-/usr/sbin/php-fpm7.4 -F
+/usr/sbin/php-fpm8.2 -F
 
 # cd /var/www/html
 # curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
