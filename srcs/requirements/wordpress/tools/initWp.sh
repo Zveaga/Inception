962642136f298e1a2c43e356be4797d71eedc51e
Rares <rares.rares.cox@gmail.com>
2024-10-11 17:51:50 +0200

    need to: 1. get the env vars inside the wp-config.php file 2. fix the wordpress errors

--- srcs/requirements/wordpress/tools/initWp.sh
+++ srcs/requirements/wordpress/tools/initWp.sh
@@ -1,8 +1,51 @@
 #!/bin/bash
 
-cd /var/www/html/
+# if [ ! -e /var/www/html/$DOMAIN_NAME/.wordpress_setup_done ]; then	
+# 	cd /var/www/html/$DOMAIN_NAME
 
-if [ ! -f ./wp-config.php ]; then 
+# 	wget https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
+# 	mv wp-cli.phar /usr/local/bin/wp
+# 	chmod +x /usr/local/bin/wp
+
+#     chown -R www-data:www-data /var/www/html/$DOMAIN_NAME
+
+# 	sleep 10
+# 	until nc -z $DB_HOST $DB_PORT; do echo 'Waiting db...' >>output && sleep 5; done
+#     # Download the WordPress core files
+# 	wp core download --allow-root
+
+# 	# Create the WordPress configuration file with the provided database credentials and host
+# 	wp config create	--allow-root \
+# 						--dbname="$DB_NAME" \
+# 						--dbuser="$WP_USER" \
+# 						--dbpass="$WP_USER_PASSWORD" \
+# 						--dbhost="$DB_HOST:$DB_PORT"
+	
+# 	# Install WordPress with the provided site information and admin credentials
+# 	wp core install	--allow-root \
+# 					--url="https://$DOMAIN_NAME/" \
+# 					--title="inception" \
+# 					--admin_user="$WP_ADMIN" \
+# 					--admin_password="$WP_ADMIN_PASSWORD" \
+# 					--admin_email="$WP_ADMIN_EMAIL" \
+# 					--skip-email
+
+# 	# Create a new WordPress user with the provided credentials and role
+# 	wp user create	"$WP_USER" "$WP_USER_EMAIL" \
+# 					--allow-root  \
+# 					--user_pass="$WP_USER_PASSWORD" \
+# 					--role=author
+
+#     touch .wordpress_setup_done
+# fi
+
+# exec "$@"
+
+
+
+#!/bin/bash
+if [ ! -f ./wp-config.php ]; then
+	cd /var/www/html/$DOMAIN_NAME
 	wget http://wordpress.org/latest.tar.gz
 	tar xfz latest.tar.gz
 	mv wordpress/* .
@@ -13,7 +56,7 @@ if [ ! -f ./wp-config.php ]; then
 	sed -i "s/localhost/$DB_HOST/g" wp-config-sample.php
 	sed -i "s/database_name_here/$DB_NAME/g" wp-config-sample.php
 	cp wp-config-sample.php wp-config.php
-	chown -R www-data:www-data /var/www/html
+	chown -R www-data:www-data /var/www/html/$DOMAIN_NAME
 	chmod -R 755 /var/www/html
 	chown www-data:www-data wp-config.php
 	chmod 644 wp-config.php
